5af61ad79462f7e9b47b143feed4bec1628da425
Camilo Burbano <camiloburbano82@gmail.com>
2023-01-23 22:06:40 -0500

    demo2 2.0

--- main.js
+++ main.js
@@ -3,16 +3,67 @@ var app = new Vue({
     data: {
         message: 'Hello Vue!',
         suma: 'Suma',
-        n1:0,
-        n2:0,
-        rta:"",
-        frutas: ['manzana','mandarina','mango', 'pera']
+        n1: 0,
+        n2: 0,
+        rta: "",
+        operaciones: ['sumar', 'restar', 'multiplicar', 'dividir'],
+        frutas: [
+            { nombre: 'manzana', cantidad: 5 },
+            { nombre: 'mandarina', cantidad: 7 },
+            { nombre: 'mango', cantidad: 10 }
+        ],
+        nombre: '',
+        cantidad: '',
+        seleccionar: '',
+        cantidadComprar: ''
     },
-    methods:{
+    methods: {
 
-        sumar(){
+        sumar() {
             this.rta = this.n1 + this.n2
+        },
+
+        restar() {
+            this.rta = this.n1 - this.n2
+        },
+
+        multiplicar() {
+            this.rta = this.n1 * this.n2
+        },
+
+        dividir() {
+            this.rta = this.n1 / this.n2
+        },
+
+        agregar() {
+            let bandera = true;
+            this.frutas.forEach(fruta => {
+                if (fruta.nombre == this.nombre) {
+                    fruta.cantidad = fruta.cantidad + this.cantidad;
+                    bandera = false;
+                }
+            })
+            if (bandera) {
+                this.frutas.push({ nombre: this.nombre, cantidad: this.cantidad })
+            }
+            this.limpiar();
+        },
+
+        comprar() {
+
+            this.frutas.forEach(fruta => {
+                if (fruta.nombre == this.seleccionar) {
+                    fruta.cantidad = fruta.cantidad - this.cantidadComprar;
+                }
+            });
+            this.limpiar();
+        },
+
+        limpiar() {
+            this.nombre = '',
+                this.cantidad = '',
+                this.seleccionar = '',
+                this.cantidadComprar = ''
         }
-       
     }
 })
